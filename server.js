56d0d6bc17e474a22e1911ef43681e419e008a31
Dustin Lodge <lodge.dustin@outlook.com>
2024-09-18 15:31:12 -0600

    updated new file

---
+++ server.js
@@ -0,0 +1,13 @@
+// express web server
+const express = require('express');
+const app = express();
+ 
+app.get('/', (req, res) => {
+  res.send("DUSTIN LODGE");
+});
+ 
+const port = 3000;
+
+app.listen(process.env.PORT || port, () => {
+  console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+});
