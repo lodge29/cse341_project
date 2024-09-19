9edd3953dcfbfb34a55155c8b9596e410c25a73f
Dustin Lodge <lodge.dustin@outlook.com>
2024-09-18 19:01:15 -0600

    organized GET, controllers, others

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const routes = require('express').Router();
+const lesson1Controller = require('../controllers/lesson1');
+
+routes.get('/', lesson1Controller.dustinRoute);
+routes.get('/lodge', lesson1Controller.lodgeRoute);
+
+module.exports = routes;
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 // express web server
 const express = require('express');
 const app = express();
-const lesson1Controller = require('./controllers/lesson1');
+// const lesson1Controller = require('./controllers/lesson1');
 
+// original code --- bulky
+/*
 app.get('/', lesson1Controller.dustinRoute);
 app.get('/lodge', lesson1Controller.lodgeRoute);
 
-// original code - bulky
-/*
+
+
 app.get('/', (req, res) => {
   res.send("dustin");
 });
@@ -16,8 +18,11 @@ app.get('/', (req, res) => {
 app.get('/lodge', (req, res) => {
     res.send("lodge");
   });
+
 */
 const port = 3000;
+
+app.use('/', require('./routes'));
 
 app.listen(process.env.PORT || port, () => {
   console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
