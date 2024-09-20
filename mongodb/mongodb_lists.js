235c823a809d07e51b5ec38fc790cf02e0cebb4d
Dustin Lodge <lodge.dustin@outlook.com>
2024-09-19 23:35:56 -0600

    mongodb connection added

---
+++ mongodb/mongodb_lists.js
@@ -0,0 +1,41 @@
+const { MongoClient } = require('mongodb');
+
+async function main(){
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+
+    const uri = "mongodb+srv://Domik29:Jk27ha80@cluster0.y6uas.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+    const client = new MongoClient(uri);
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+ 
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+/*
+ * Print the names of all available databases
+ * @param {MongoClient} client A MongoClient that is connected to a cluster
+ */
+
+
+
+main().catch(console.error);
+
+async function listDatabases(client) {
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
